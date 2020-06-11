103537843393ee3c9bce62747fa1ab543a82ab67
Vladislav Vulchev <vvladoh@gmail.com>
2020-06-11 15:41:23 +0300

    One Code to rule them all

--- MarsLanderEpisode2/marsLanderEpisode2.cpp
+++ MarsLanderEpisode2/marsLanderEpisode2.cpp
@@ -1,3 +1,9 @@
+#pragma GCC optimize("O3","unroll-loops","omit-frame-pointer","inline") //Optimization flags
+#pragma GCC option("arch=native","tune=native","no-zero-upper") //Enable AVX
+#pragma GCC target("avx")  //Enable AVX
+#include <x86intrin.h> //AVX/SSE Extensions
+#include <bits/stdc++.h> //All main STD libraries
+
 #include <iostream>
 #include <string>
 #include <vector>
